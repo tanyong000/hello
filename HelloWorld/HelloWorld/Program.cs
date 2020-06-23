5de7706ae46400e88611de0dbbba43f1f52f4141
tanyong000 <tanyong000@outlook.con>
2020-06-23 21:57:18 +0800

    My first git project.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1;
+            int c = 2;
+            Console.WriteLine(a+b);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
